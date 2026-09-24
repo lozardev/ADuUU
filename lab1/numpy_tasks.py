74fd545084989fdea4b449d7be6e0ea1f1c97621
lozardev <a.dremkov25@gmail.com>
2026-09-24 14:44:23 +0500

    lab1 done

--- lab1/numpy_tasks.py
+++ lab1/numpy_tasks.py
@@ -2,61 +2,124 @@
 
 import numpy as np
 from grader_contracts.numpy_tasks import (
-    BinarizeInput, ChessInput, EllipseInput, MatrixInput, MatrixStatistics,
-    MatrixVectorBatchInput, OneHotInput, RandomMatrixInput, RectangleInput,
-    TimeSeriesInput, TimeSeriesStatistics,
+    BinarizeInput,
+    ChessInput,
+    EllipseInput,
+    MatrixInput,
+    MatrixStatistics,
+    MatrixVectorBatchInput,
+    OneHotInput,
+    RandomMatrixInput,
+    RectangleInput,
+    TimeSeriesInput,
+    TimeSeriesStatistics,
 )
 
 
-def sum_prod(data: MatrixVectorBatchInput) -> np.ndarray:
+def sum_prod(data: MatrixVectorBatchInput) -> np.np.ndarray:
     matrices, vectors = data.matrices, data.vectors
-    raise NotImplementedError  # TODO
+    return np.einsum("pij,pjk->ik", matrices, vectors)
 
 
 def binarize(data: BinarizeInput) -> np.ndarray:
     matrix, threshold = data.matrix, data.threshold
-    raise NotImplementedError  # TODO
+    return np.where(matrix > threshold, 1, 0)
 
 
 def unique_rows(data: MatrixInput) -> list[list[float]]:
     matrix = data.matrix
-    raise NotImplementedError  # TODO
+    return [np.unique(row).tolist() for row in matrix]
 
 
 def unique_columns(data: MatrixInput) -> list[list[float]]:
     matrix = data.matrix
-    raise NotImplementedError  # TODO
+    return [np.unique(row).tolist() for row in matrix.T]
 
 
 def matrix_statistics(data: RandomMatrixInput) -> MatrixStatistics:
-    rows, columns, mean, std, seed = data.rows, data.columns, data.mean, data.std, data.seed
-    raise NotImplementedError  # TODO
+    rows, columns, mean, std, seed = (
+        data.rows,
+        data.columns,
+        data.mean,
+        data.std,
+        data.seed,
+    )
+
+    rng = np.random.default_rng(seed=52)
+    matrix = rng.normal(loc=mean, scale=std, size=(rows, columns))
+
+    row_means = matrix.mean(axis=1)
+    col_means = matrix.mean(axis=0)
+    row_vars = matrix.var(axis=1)
+    col_vars = matrix.var(axis=0)
+
+    return MatrixStatistics(matrix, row_means, col_means, row_vars, col_vars)
 
 
 def chess(data: ChessInput) -> np.ndarray:
     rows, columns, first, second = data.rows, data.columns, data.first, data.second
-    raise NotImplementedError  # TODO
+
+    i = np.arange(rows)[:, None]
+    j = np.arange(columns)
+
+    return np.where((i + j) % 2 == 0, first, second)
 
 
 def draw_rectangle(data: RectangleInput) -> np.ndarray:
     width, height = data.width, data.height
     image_height, image_width = data.image_height, data.image_width
     shape_color, background_color = data.shape_color, data.background_color
-    raise NotImplementedError  # TODO
+
+    image = np.full((image_height, image_width, 3), background_color)
+    start_y = (image_height - height) // 2
+    start_x = (image_width - width) // 2
+    image[start_y : start_y + height, start_x : start_x + width] = shape_color
+
+    return image
 
 
 def draw_ellipse(data: EllipseInput) -> np.ndarray:
     semi_axis_x, semi_axis_y = data.semi_axis_x, data.semi_axis_y
     image_height, image_width = data.image_height, data.image_width
     shape_color, background_color = data.shape_color, data.background_color
-    raise NotImplementedError  # TODO
+
+    image = np.full((image_height, image_width, 3), background_color)
+    y0 = (image_height - 1) / 2
+    x0 = (image_width - 1) / 2
+    y, x = np.ogrid[:image_height, :image_width]
+    mask = ((x - x0) ** 2) / (semi_axis_x**2) + ((y - y0) ** 2) / (semi_axis_y**2) <= 1
+    image[mask] = shape_color
+
+    return image
 
 
 def analyze_time_series(data: TimeSeriesInput) -> TimeSeriesStatistics:
     values, window = data.values, data.window
-    raise NotImplementedError  # TODO
+
+    mean = float(np.mean(values))
+    var = float(np.var(values))
+    std = float(np.std(values))
+
+    max_test = (values[1:-1] > values[:-2]) & (values[1:-1] > values[2:])
+    local_max = np.where(max_test)[0] + 1
+
+    min_test = (values[1:-1] < values[:-2]) & (values[1:-1] < values[2:])
+    local_min = np.where(min_test)[0] + 1
+
+    new_row = np.convolve(values, np.ones(window) / window, mode="valid")
+
+    return TimeSeriesStatistics(mean, var, std, local_max, local_min, new_row)
 
 
 def one_hot(data: OneHotInput) -> np.ndarray:
     labels, class_count = data.labels, data.class_count
-    raise NotImplementedError  # TODO
+
+    if class_count:
+        matrix = np.zeros((len(labels), class_count), dtype=int)
+    else:
+        matrix = np.zeros((len(labels), max(labels) + 1), dtype=int)
+
+    for row, e in enumerate(labels):
+        matrix[row, e] = 1
+
+    return matrix
--- lab1/python_basics.py
+++ lab1/python_basics.py
@@ -1,43 +1,117 @@
 """Заготовки задач на базовый Python."""
 
-from grader_contracts.python_basics import PositiveIntegerInput, TextInput, VectorPairInput
+from grader_contracts.python_basics import (
+    PositiveIntegerInput,
+    TextInput,
+    VectorPairInput,
+)
 
 
 def count_vowels(data: TextInput) -> int:
     text = data.value
-    raise NotImplementedError  # TODO
+    vowels = set("aeiou")
+    return sum(1 for char in text.lower() if char in vowels)
 
 
 def has_unique_characters(data: TextInput) -> bool:
     text = data.value
-    raise NotImplementedError  # TODO
+    return len(text) == len(set(text))
 
 
 def count_one_bits(data: PositiveIntegerInput) -> int:
     number = data.value
-    raise NotImplementedError  # TODO
+    return bin(number).count("1")
 
 
 def multiplicative_persistence(data: PositiveIntegerInput) -> int:
     number = data.value
-    raise NotImplementedError  # TODO
+    steps = 0
+
+    while number >= 10:
+        temp = 1
+        for e in str(temp):
+            temp *= int(e)
+        number = temp
+        steps += 1
+
+    return steps
 
 
 def mse(data: VectorPairInput) -> float:
     predicted, expected = data.predicted, data.expected
-    raise NotImplementedError  # TODO
+    err = 0
+    n = len(predicted)
+
+    for i in range(n):
+        diff = predicted[i] - expected[i]
+        err += diff**2
+
+    return err / n
 
 
 def prime_factorization(data: PositiveIntegerInput) -> str:
     number = data.value
-    raise NotImplementedError  # TODO
+
+    if number <= 1:
+        return f"({number})"
+
+    result = ""
+
+    if number % 2 == 0:
+        power = 0
+        while number % 2 == 0:
+            number //= 2
+            power += 1
+        result += f"(2**({power})" if power > 1 else f"({2})"
+
+    div = 3
+    while div**2 <= number:
+        if number % div == 0:
+            power = 0
+            while number % div == 0:
+                number //= div
+                power += 1
+            result += f"({div}**{power})" if power > 1 else f"({div})"
+        div += 2
+
+    if number > 1:
+        result += f"({number})"
+
+    return result
 
 
 def pyramid(data: PositiveIntegerInput) -> int | str:
     cube_count = data.value
-    raise NotImplementedError  # TODO
+    total = 0
+    k = 0
+
+    while cube_count > total:
+        k += 1
+        sum += k**2
+
+    return k if cube_count == total else "It is impossible"
 
 
 def is_balanced_number(data: PositiveIntegerInput) -> bool:
-    number = data.value
-    raise NotImplementedError  # TODO
+    number = str(data.value)
+    l = len(number)
+
+    if l % 2 <= 2:
+        return True
+
+    if l % 2 == 0:
+        left = number[: l // 2 - 1]
+        right = number[l // 2 + 1 :]
+    else:
+        left = number[: l // 2]
+        right = number[l // 2 + 1 :]
+
+    left_sum = 0
+    for e in left:
+        left_sum += e
+
+    right_sum = 0
+    for e in right:
+        right_sum += e
+
+    return left_sum == right_sum
